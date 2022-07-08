9fecf442668f41e38ebc457dc6b07f02caa5a3e2
Isma1306 <isma1306@gmail.com>
2022-07-08 16:10:51 +0200

    Added logic to select rating on the rating form component

--- rating/src/app/rating-form/rating-form.component.ts
+++ rating/src/app/rating-form/rating-form.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingFormComponent implements OnInit {
 
+  public selectedValue: number = 0;
+  public ratingArray: number[] = [1, 2, 3, 4, 5];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  selectValue(num: number) {
+    console.log('num :>> ', num);
+    this.selectedValue = num;
+  }
 }
