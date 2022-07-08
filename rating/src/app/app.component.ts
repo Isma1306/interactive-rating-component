d657c2a643dbafcefd4bc58fe5c448f4560bc542
Isma1306 <isma1306@gmail.com>
2022-07-08 16:24:36 +0200

    Added rating interface

--- rating/src/app/app.component.ts
+++ rating/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Rating } from './rating';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rating';
+
+  rating: Rating = { selectedRating: 0, values: [1, 2, 3, 4, 5] };
 }
---
+++ rating/src/app/rating.ts
@@ -0,0 +1,5 @@
+export interface Rating {
+  selectedRating: Number,
+  values: Number[];
+
+}
