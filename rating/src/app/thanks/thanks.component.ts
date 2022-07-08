624999ec4d6ad0309dacb8d472e30e72e14f9d18
Isma1306 <isma1306@gmail.com>
2022-07-08 17:57:47 +0200

    fixed some details on the styling

--- rating/src/app/thanks/thanks.component.ts
+++ rating/src/app/thanks/thanks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-thanks',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThanksComponent implements OnInit {
 
-  constructor() { }
+  @Input() value: Number = 0;
 
   ngOnInit(): void {
   }
